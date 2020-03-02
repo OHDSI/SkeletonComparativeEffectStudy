4e1d33dfc7c8e50c9887c5685593e9f91449e53c
Gowtham Rao <grao9@its.jnj.com>
2020-03-02 09:41:15 -0500

    Added new function getFollowUpDistribution
    also added minor corrections to documentaiton of function getPreferenceScoreDistribution

--- R/DataPulls.R
+++ R/DataPulls.R
@@ -96,6 +96,57 @@
   return(result)
 }
 
+
+.getCmFollowUpDist <- function(connectionDetails = NULL,
+                               cmFollowUpDist = NULL,
+                               databaseSchema = NULL,
+                               targetId,
+                               comparatorId,
+                               outcomeId,
+                               databaseId,
+                               analysisId) {
+  if (!is.null(connectionDetails)) {
+    connection <- DatabaseConnector::connect(connectionDetails)
+    sql <-   "SELECT *
+              FROM @databaseSchema.cm_follow_up_dist
+              WHERE target_id = @targetId
+              	and comparator_id = @comparatorId
+              	and outcome_id = @outcomeId
+              	and analysis_id = @analysisId
+              	and database_id = '@databaseId';"
+    
+    sql <-
+      SqlRender::render(
+        sql = sql,
+        databaseSchema = databaseSchema,
+        targetId = targetId,
+        comparatorId = comparatorId,
+        outcomeId = outcomeId,
+        analysisId = analysisId,
+        databaseId = databaseId
+      )
+    sql <-
+      SqlRender::translate(sql = sql,
+                           targetDialect = connection@dbms)
+    result <-
+      DatabaseConnector::querySql(
+        connection = connection,
+        sql = sql,
+        snakeCaseToCamelCase = TRUE
+      )
+  } else if (!is.null(cmFollowUpDist)) {
+    result <- cmFollowUpDist %>%
+      dplyr::filter(
+        targetId %in% !!targetId,
+        comparatorId %in% !!comparatorId,
+        outcomeId %in% !!outcomeId,
+        databaseId %in% !!databaseId,
+        analysisId %in% !!analysisId
+      )
+  }
+  return(result)
+}
+
 #' Get the preference score distribution for plotting
 #'
 #' @details
@@ -112,7 +163,7 @@
 #' databaseSchema in function call instead.
 #' 
 #' If the comparative effectiveness results are stored as a R dataframe object,
-#' please make sure connectionDetails is NULL (Defaul value). The function 
+#' please make sure connectionDetails is NULL (Default value). The function 
 #' will expect data in R dataframe. This R dataframe object is expected to follow 
 #' camelCase for both object names and variable names. 
 #'  
@@ -127,12 +178,12 @@
 #'                             data from preference_score_dist table in comparative effectiveness data model.
 #' @param exposureOfInterest   (optional) A R-dataFrame object with fields named in camelCase containing
 #'                             data from exposure_of_interest table in comparative effectiveness data model.
-#' @return                     Tibble with fields preference score, density, cohort-name,
-#'                             group for identifying if the cohort is target vs comparator.
+#' @return                     Tibble with fields preference score, density, cohort-name, group.
+#'                             group is for identifying if the cohort is target vs comparator.
 #' @examples
 #' \dontrun{
 #'
-#' #If you're data is in data frames, provide connectionDetails
+#' #If your data is in data frames, provide connectionDetails
 #' connectionDetails <- createConnectionDetails(dbms = "postgresql",
 #'                                              user = "joe",
 #'                                              password = "secret",
@@ -226,6 +277,127 @@ getPreferenceScoreDistribution <- function(connectionDetails = NULL,
   result <- preferenceScoreDist %>%
     dplyr::left_join(y = exposureOfInterest, by = c("group" = "group")) %>%
     dplyr::mutate(group = SqlRender::camelCaseToTitleCase(group))
+  
+  return(result)
+}
+
+
+
+#' Get followup distribution data.
+#'
+#' @details
+#' Get follow-up distribution data from data stored in OHDSI comparative effectiveness data model,
+#' The output of this function  maybe used to create plots or tables
+#' that uses the follow-up distribution data.
+#' 
+#' If the comparative effectiveness results are stored in a 
+#' relational database server, then please provide connection details 
+#' using DatabaseConnector::createConnectionDetails. If connectionDetails 
+#' parameter is not null, the function will attempt to connect to 
+#' relational database server, and ignore using R dataframe object. 
+#' Note: schema parameter in createConnectionDetails is not used. We use
+#' databaseSchema in function call instead.
+#' 
+#' If the comparative effectiveness results are stored as a R dataframe object,
+#' please make sure connectionDetails is NULL (Default value). The function 
+#' will expect data in R dataframe. This R dataframe object is expected to follow 
+#' camelCase for both object names and variable names.
+#' 
+#'
+#' @template optionalConnectionDetails
+#' @template optionalDatabaseSchema
+#' @template targetId
+#' @template comparatorId
+#' @template databaseId
+#' @template analysisId
+#' @template outcomeId
+#' @param cmFollowUpDist       (optional) A R-dataFrame object with fields named in camelCase containing
+#'                             data from cm_follow_up_dist table in comparative effectiveness data model.
+#' @return                     Tibble with fields Id, Min, P10, P25, Median, P75, P90,Max, group.
+#'                             group is for identifying if the cohort is target vs comparator.
+#'
+#' @examples
+#' \dontrun{
+#' #If your data is in data frames, provide connectionDetails
+#' connectionDetails <- createConnectionDetails(dbms = "postgresql",
+#'                                              user = "joe",
+#'                                              password = "secret",
+#'                                              server = "myserver")
+#' #If connectionDetails is provided, function will attempt to read data from database
+#' example1 <- getFollowUpDaysDistribution(connectionDetails = connectionDetails,
+#'                                         databaseSchema = 'database.schema',
+#'                                         targetId = 100,
+#'                                         comparatorId = 200,
+#'                                         outcomeId = 111,
+#'                                         databaseId = 'HCUP data',
+#'                                         analysisId = 2)
+#' #If no connectionDetails is provided, then function will check for data in dataFrame
+#' example2 <- getFollowUpDaysDistribution(cmFollowUpDist = cmFollowUpDist,
+#'                                         targetId = 100,
+#'                                         comparatorId = 200,
+#'                                         outcomeId = 111,
+#'                                         databaseId = 'HCUP data',
+#'                                         analysisId = 2)
+#' }
+#' @export
+#'
+getFollowUpDistribution <- function(connectionDetails = NULL,
+                                    databaseSchema = NULL,
+                                    cmFollowUpDist = NULL,
+                                    targetId,
+                                    comparatorId,
+                                    outcomeId,
+                                    databaseId,
+                                    analysisId) {
+  errorMessage <- checkmate::makeAssertCollection()
+  checkmate::assertInt(targetId, add = errorMessage)
+  checkmate::assertInt(comparatorId, add = errorMessage)
+  checkmate::assertInt(outcomeId, add = errorMessage)
+  checkmate::assertInt(analysisId, add = errorMessage)
+  checkmate::assertScalar(databaseId, add = errorMessage)
+  checkmate::assertCharacter(databaseId, add = errorMessage)
+  
+  if (!is.null(connectionDetails)) {
+    checkmate::assertCharacter(databaseSchema)
+    checkmate::reportAssertions(errorMessage)
+    
+  } else if (is.null(connectionDetails)) {
+    checkmate::assertDataFrame(cmFollowUpDist, add = errorMessage)
+    checkmate::reportAssertions(errorMessage)
+  } else {
+    stop(
+      "No connection details for an RDMS provided.
+         No R-data frame object cmFollowUpDist provided"
+    )
+  }
+  
+  .prepareData <- function(data, type) {
+    data %>%
+      dplyr::select(dplyr::starts_with(type)) %>%
+      purrr::map_at(names(.)[stringr::str_detect(string = names(.), pattern = ".*(?:(?!Id).).$")], #not ends with Id regex
+                    scales::comma, big.mark = ",") %>%
+      tidyr::as_tibble() %>% #map_at returns a list, not a dataframe.
+      dplyr::rename_all( ~ stringr::str_replace(., type, "")) %>%
+      dplyr::rename_all( ~ stringr::str_replace(., "Days", "")) %>%
+      dplyr::mutate(group = type)
+  }
+  
+  cmFollowUpDist <-
+    .getCmFollowUpDist(
+      connectionDetails = connectionDetails,
+      databaseSchema = databaseSchema,
+      cmFollowUpDist = cmFollowUpDist,
+      targetId = targetId,
+      comparatorId = comparatorId,
+      outcomeId = outcomeId,
+      databaseId = databaseId,
+      analysisId = analysisId
+    )
+  
+  result <- dplyr::bind_rows(
+    .prepareData(data = cmFollowUpDist, type = "target"),
+    .prepareData(data = cmFollowUpDist, type = "comparator")
+  )
   
   return(result)
 }
