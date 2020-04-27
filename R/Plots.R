0c234e5a194ce2f629ebacfe8c920e2b651554f1
Gowtham Rao <grao9@its.jnj.com>
2020-04-27 09:25:25 -0400

    Added new visualization function for covariate balance scatter
    Continuing https://github.com/OHDSI/SkeletonComparativeEffectStudy/issues/27
    
    https://github.com/OHDSI/SkeletonComparativeEffectStudy/issues/27#issuecomment-619984123

---
+++ R/Plots.R
@@ -0,0 +1,72 @@
+# @file Plots
+#
+# Copyright 2020 Observational Health Data Sciences and Informatics
+#
+# This file is part of SkeletonComparativeEffectStudy
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+
+.getDefaultGgplotTheme <- function() {
+  ggplot2::theme_set(ggplot2::theme_minimal())
+  ggplot2::theme_update(
+    panel.grid = ggplot2::element_blank(),
+    legend.title = ggplot2::element_blank(),
+    legend.position = 'top'
+  )
+  return(ggplot2::theme_get())
+}
+
+#' Get the covariate balance scatter plot for data covariateBalance object.
+#'
+#' @details
+#' This function creates the covariate balance scatter plot for data in 
+#' covariateBalance object. The covariateBalance object is created from OHDSI
+#' comparative effectiveness data model by function \code{getCovariateBalance}. 
+#' 
+#' (Advanced) The output is a ggplot object. It maybe modified to change color, title, label etc.
+#'
+#' @param covariateBalance  A R-dataFrame covariateBalance object that was derived by 
+#'                          function \code{getCovariateBalance}
+#' @param fileName          (optional) Name of the file where the plot should be saved, for example 'plot.png'.
+#'                          See the function \code{ggsave} in the ggplot2 package for supported file
+#'                          formats.
+#' @return                  ggplot object. if fileName is provided, a ggsave will be used to save the object.
+#' @examples
+#' \dontrun{
+#' example1 <- plotCovariateBalanceScatter(covariateBalance = covariateBalance)
+#' } 
+#' @export
+plotCovariateBalanceScatter <- function(covariateBalance = NULL,
+                                fileName = NULL
+) {
+  if (is.null(covariateBalance)) {
+    stop("Must provide covariateBalance object.")
+  }
+  limits <- c(min(c(balance$absBeforeMatchingStdDiff, balance$absAfterMatchingStdDiff),
+                  na.rm = TRUE),
+              max(c(balance$absBeforeMatchingStdDiff, balance$absAfterMatchingStdDiff),
+                  na.rm = TRUE))
+  plot <- ggplot2::ggplot(balance, 
+                          ggplot2::aes(x = absBeforeMatchingStdDiff, y = absAfterMatchingStdDiff)) +
+    ggplot2::geom_point(color = rgb(0, 0, 0.8, alpha = 0.5), shape = 16, size = 2) +
+    ggplot2::geom_abline(slope = 1, intercept = 0, linetype = "dashed", alpha = 0.2) +
+    ggplot2::geom_hline(yintercept = 0, na.rm = T) +
+    ggplot2::geom_hline(yintercept = 0.1, linetype = "dashed", size = 0.1, alpha = 0.2, na.rm = T) +
+    ggplot2::geom_vline(xintercept = 0, na.rm = T) +
+    ggplot2::scale_x_continuous("Before stratification", limits = limits) +
+    ggplot2::scale_y_continuous("After stratification", limits = limits) + 
+    ggplot2::theme_light()
+  if (!is.null(fileName)) {
+    ggplot2::ggsave(fileName, plot, width = 5, height = 3.5, dpi = 400)
+  }
+  return(plot)
+}
